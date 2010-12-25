acc6ee545134d10b7cc01ff0971eaface8765691
spearwolf <wolfger@spearwolf.de>
2010-12-25 15:59:41 +0100

    integrated compass/sass

---
+++ config/compass.rb
@@ -0,0 +1,8 @@
+# This is the Compass configuration file. The configuration file for nanoc is 
+# named “config.yaml”.
+
+project_path = File.join(File.dirname(__FILE__), '..')
+http_path    = '/'
+output_style = :compressed
+sass_dir     = 'content/style'
+css_dir      = 'output/style'
